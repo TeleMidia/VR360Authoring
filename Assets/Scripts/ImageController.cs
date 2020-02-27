db867c4f9833cb93627d1fbbd919857e3694a1f7
paulorcmendes <mendesdpn@gmail.com>
2020-02-27 15:08:00 -0300

    loading and destroying image

---
+++ Assets/Scripts/ImageController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float start_time, duration;
+
+    public void LoadImage(string file_path, float width, float height)
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        Texture2D texture = new Texture2D(2, 2);
+        Material material = new Material(Shader.Find("Unlit/Texture"));
+
+        byte[] fileData = File.ReadAllBytes(file_path);
+        texture.LoadImage(fileData);
+        material.mainTexture = texture;
+        GetComponent<MeshRenderer>().material = material;
+        this.transform.localScale = new Vector3(width, height, 1);
+        
+    }
+    public void Play()
+    {
+        Invoke("ActiveRenderer", start_time);
+    }
+    void ActiveRenderer()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        Invoke("DeactiveRenderer", duration);
+    }
+    void DeactiveRenderer()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/MultimediaController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MultimediaController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public VideoPlayer video360;
+    public GameObject imagePrefab;
+    private List<GameObject> other_media;
+    void Start()
+    {
+        other_media = new List<GameObject>();
+        video360.started += StartOtherMedia;
+        AddImage(5f, 3f, @"C:\Users\paulo\Pictures\WebMedia\31-10-2019-505.jpg", 16f, 9f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(video360.time);
+        if (Input.GetMouseButtonDown(0))
+        {
+            video360.Play();
+        }
+    }
+    void StartOtherMedia(VideoPlayer source)
+    {
+        foreach(GameObject media in other_media)
+        {
+            media.GetComponent<ImageController>().Play();
+        }
+    }
+    void AddImage(float start_time, float duration, string url, float width, float height)
+    {
+        GameObject newImage = Instantiate(imagePrefab);
+        newImage.GetComponent<ImageController>().start_time = start_time;
+        newImage.GetComponent<ImageController>().duration = duration;
+        newImage.GetComponent<ImageController>().LoadImage(url, width, height);
+        other_media.Add(newImage);
+    }
+}
