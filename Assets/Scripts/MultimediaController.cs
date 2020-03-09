206c0650348fd5cf2de5bcc902d655829ae3efdb
paulorcmendes <mendesdpn@gmail.com>
2020-03-09 11:32:52 -0300

    reading from xml file

--- Assets/Scripts/MultimediaController.cs
+++ Assets/Scripts/MultimediaController.cs
@@ -20,8 +20,8 @@ public class MultimediaController : MonoBehaviour
     private event MyHandler Port;
     private event MyHandler End;
     void Start()
-    {   
-        video1 = AddVideo360("C:/Users/paulo/Downloads/360_VR Master Series _ Free Download _ Crystal Shower Falls.mp4");
+    {
+        /*video1 = AddVideo360("C:/Users/paulo/Downloads/360_VR Master Series _ Free Download _ Crystal Shower Falls.mp4");
         video2 = AddVideo360("C:/Users/paulo/Downloads/Best VR 360 Video.mp4");
 
         video1.GetComponent<Video360Controller>().AddSubtitle(textPrefab, r: 10, theta: 25, phi: 0, file_path: @"C:\Users\paulo\Downloads\jumanji-the-next-level_HI_english-2156994\Jumanji.The.Next.Level.2019.BDRip.XviD.AC3-EVO-HI.srt");
@@ -73,7 +73,9 @@ public class MultimediaController : MonoBehaviour
 
         SetAsInitial(this.video1);
         //this.Port();
+        */
 
+        LoadXmlFile("C:/Users/paulo/Documents/VR360Video/Assets/360PresentationExample.xml");
         GameObject[] videos360 = GameObject.FindGameObjectsWithTag("Video360");
         foreach(GameObject video360 in videos360)
         {
@@ -147,11 +149,21 @@ public class MultimediaController : MonoBehaviour
                     {
                         volume = float.Parse(volume_node.Value);
                     }
-                    GameObject new_video360 = AddVideo360(src:src, volume:volume);
-                    AddAdditionalMedia(body_child, new_video360 ,movements, positions, video360_objects);
-                    video360_objects.Add(src, new_video360);
+                    GameObject new_video360 = AddVideo360(src:src, volume:volume);                    
+                    video360_objects.Add(id, new_video360);
                 }
             }
+            //adding additional media to video360 files
+            foreach (XmlNode body_child in body.ChildNodes)
+            {
+                if (body_child.Name.Equals("video360"))
+                {
+                    string id = body_child.Attributes.GetNamedItem("id").Value;
+                    GameObject new_video360 = video360_objects[id];
+                    AddAdditionalMedia(body_child, new_video360, movements, positions, video360_objects);
+                }
+            }
+
             var port = document.DocumentElement.SelectSingleNode("//presentation360/body/port");
 
             SetAsInitial(video360_objects[port.Attributes.GetNamedItem("component").Value]);
@@ -160,7 +172,60 @@ public class MultimediaController : MonoBehaviour
 
     public void AddAdditionalMedia(XmlNode video360node, GameObject video360, Dictionary<string, Movement> movements, Dictionary<string, Position> positions, Dictionary<string, GameObject> video360_objects)
     {
+        foreach (XmlNode mediaNode in video360node.ChildNodes)
+        {
+            if (mediaNode.Name.Equals("media"))
+            {
+                string type = mediaNode.Attributes.GetNamedItem("type").Value;
+                float start_time = mediaNode.Attributes.GetNamedItem("start_time") != null? float.Parse(mediaNode.Attributes.GetNamedItem("start_time").Value): 0;
+                float duration = mediaNode.Attributes.GetNamedItem("duration") != null ? float.Parse(mediaNode.Attributes.GetNamedItem("duration").Value) : 0;
+                float volume = mediaNode.Attributes.GetNamedItem("volume") != null ? float.Parse(mediaNode.Attributes.GetNamedItem("volume").Value) : 0;
+                bool follow_camera = mediaNode.Attributes.GetNamedItem("follow_camera") != null ? bool.Parse(mediaNode.Attributes.GetNamedItem("follow_camera").Value) : false;
+                bool loop = mediaNode.Attributes.GetNamedItem("loop") != null ? bool.Parse(mediaNode.Attributes.GetNamedItem("loop").Value) : false;
+                string src = mediaNode.Attributes.GetNamedItem("src") != null ? mediaNode.Attributes.GetNamedItem("src").Value : "";
+                string text = mediaNode.Attributes.GetNamedItem("text") != null ? mediaNode.Attributes.GetNamedItem("text").Value : "";
+                Movement movement = mediaNode.Attributes.GetNamedItem("movement") != null ? movements[mediaNode.Attributes.GetNamedItem("movement").Value] : null;
+                GameObject controller = mediaNode.Attributes.GetNamedItem("anchor") != null ? video360_objects[mediaNode.Attributes.GetNamedItem("anchor").Value] : null;
+                Position position = mediaNode.Attributes.GetNamedItem("position") != null ? positions[mediaNode.Attributes.GetNamedItem("position").Value] : null; ;
+                float r = 0, theta = 0, phi = 0;                
+                
+                if(position != null)
+                {
+                    r = position.r;
+                    theta = position.theta;
+                    phi = position.phi;
+                }
 
+                switch (type)
+                {
+                    case "image":
+                        video360.GetComponent<Video360Controller>().AddMedia(imagePrefab, start_time:start_time, duration:duration,
+                            r: r, theta: theta, phi: phi, movement: movement, file_path:src, follow_camera:follow_camera);
+                        break;
+                    case "audio":
+                        video360.GetComponent<Video360Controller>().AddMedia(audioPrefab, start_time: start_time, duration: duration,
+                           r: r, theta: theta, phi: phi, movement: movement, file_path: src, follow_camera: follow_camera, 
+                           volume:volume, loop:loop);
+                        break;
+                    case "video":
+                        video360.GetComponent<Video360Controller>().AddMedia(videoPrefab, start_time: start_time, duration: duration,
+                           r: r, theta: theta, phi: phi, movement: movement, file_path: src, follow_camera: follow_camera,
+                           volume: volume, loop: loop);
+                        break;
+                    case "text":
+                        video360.GetComponent<Video360Controller>().AddMedia(textPrefab, start_time: start_time, duration: duration,
+                           r: r, theta: theta, phi: phi, movement: movement, follow_camera: follow_camera, text:text);
+                        break;
+                    case "subtitle":
+                        video360.GetComponent<Video360Controller>().AddSubtitle(textPrefab, file_path:src, r: r, theta: theta, phi: phi);
+                        break;
+                    case "video360anchor":
+                        video360.GetComponent<Video360Controller>().AddMedia(video360PreviewPrefab, start_time: start_time, duration: duration,
+                           r: r, theta: theta, phi: phi, movement: movement, follow_camera: follow_camera, controller:controller);
+                        break;
+                }
+            }
+        }
     }
 
 
@@ -175,34 +240,36 @@ public class MultimediaController : MonoBehaviour
                 Movement new_mov;
                 var id = mov_node.Attributes.GetNamedItem("id").Value;
                 var type = mov_node.Attributes.GetNamedItem("type").Value;
-                switch (type)
-                {
-                    case "circular":
-                        new_mov = new CircularMovement();
-                        break;
-                    default:
-                        new_mov = new LinearMovement();
-                        break;
-                }
-
+                float r=0, theta=0, phi=0, duration=1;
                 foreach (XmlAttribute att in mov_node.Attributes)
                 {
                     switch (att.Name)
                     {
                         case "r":
-                            new_mov.delta_r = float.Parse(att.Value);
+                            r = float.Parse(att.Value);
                             break;
                         case "theta":
-                            new_mov.delta_theta = float.Parse(att.Value);
+                            theta = float.Parse(att.Value);
                             break;
                         case "phi":
-                            new_mov.delta_phi = float.Parse(att.Value);
+                            phi= float.Parse(att.Value);
                             break;
                         case "duration":
-                            new_mov.duration = float.Parse(att.Value);
+                            duration = float.Parse(att.Value);
                             break;
                     }
                 }
+                switch (type)
+                {
+                    case "circular":
+                        new_mov = new CircularMovement(r:r, theta:theta, phi:phi, duration:duration);
+                        break;
+                    default:
+                        new_mov = new LinearMovement(r: r, theta: theta, phi: phi, duration: duration);
+                        break;
+                }
+
+                
                 movements.Add(id, new_mov);
             }
         }
